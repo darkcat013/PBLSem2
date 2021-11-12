26b3c994481ebebe48c83617006aa57bbbb1401b
Viorel Noroc <viorel.noroc@amdaris.com>
2021-11-12 12:07:47 +0200

    New project with identity

---
+++ Construx.App/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Construx.App.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
