16c1f74437c595df8d259db7f84a890f60a99edc
Viorel Noroc <viorel.noroc@amdaris.com>
2021-12-01 22:30:37 +0200

    added services seed

---
+++ Construx.App/Data/Seeds/ServicesSeed.cs
@@ -0,0 +1,30 @@
+﻿using Construx.App.Domain.Entities;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Construx.App.Data.Seeds
+{
+    public class ServicesSeed
+    {
+        public static async Task Seed(ApplicationDbContext dbContext)
+        {
+            var services = dbContext.Services;
+            if(!services.Any())
+            {
+                List<Service> serviceList = new List<Service>
+                {
+                    new Service()
+                    {
+                        CategoryId = 1,
+                    }
+                };
+                foreach(var service in services)
+                {
+                    await services.AddAsync(service);
+                    await dbContext.SaveChangesAsync();
+                }    
+            }
+        }
+    }
+}
